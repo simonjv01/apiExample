a7ea3b12bd00223ed5579c6774d4ce1c742edff2
Simon Vargas <sjvargas01@gmail.com>
2025-02-09 21:57:29 -0500

    deleted files

--- api-example.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-response = requests.get('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
-
-data = response.json()
-
-print(json.dumps(data, indent=4))
